752d43f9705caa0de751319abfd10ad9f47f6c85
hanwoo7726 <hanwoo726@naver.com>
2026-08-03 13:24:43 +0900

    "feat: Hub 및 HubRoute 엔티티 구현"

---
+++ hub-service/src/main/java/com/sparta/hub/entity/HubRoute.java
@@ -0,0 +1,55 @@
+package com.sparta.hub.entity;
+
+import com.sparta.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Table(name = "p_hub_routes")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class HubRoute extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    // 출발 허브
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "departure_hub_id", nullable = false)
+    private Hub departureHub;
+
+    // 도착 허브
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "arrival_hub_id", nullable = false)
+    private Hub arrivalHub;
+
+    // 예상 소요 시간
+    @Column(name = "duration_minutes", nullable = false)
+    private Integer durationMinutes;
+
+    // 이동 거리
+    @Column(name = "distance_km", nullable = false, precision = 10, scale = 2)
+    private BigDecimal distanceKm;
+
+    public HubRoute(Hub departureHub, Hub arrivalHub, Integer durationMinutes,
+                    BigDecimal distanceKm){
+        this.departureHub = departureHub;
+        this.arrivalHub = arrivalHub;
+        this.durationMinutes = durationMinutes;
+        this.distanceKm = distanceKm;
+
+    }
+
+    public void updateRoute(Integer durationMinutes, BigDecimal distanceKm){
+        this.durationMinutes = durationMinutes;
+        this.distanceKm = distanceKm;
+    }
+
+
+}
