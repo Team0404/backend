e2e6661c35adcdecaccaebb09ab5e7035ff8c5a8
dduddupark <develop.dduddu@gmail.com>
2026-08-07 12:35:08 +0900

    fix: ProductClient name 변경 (product-service > company-service)

--- order-service/src/main/java/com/sparta/order/client/ProductClient.java
+++ order-service/src/main/java/com/sparta/order/client/ProductClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.UUID;
 
-@FeignClient(name = "product-service", contextId = "productClient", path = "/api/v1/products")
+@FeignClient(name = "company-service", contextId = "productClient", path = "/api/v1/products")
 public interface ProductClient {
 
     @GetMapping("/{id}")
